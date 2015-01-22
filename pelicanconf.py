d53bb5802016354f55f933317d997ec826ea9827
Fei Gao <feigao.chn@gmail.com>
2015-01-22 20:51:45 +0800

    set up

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,19 +20,41 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ('Pelican', 'http://getpelican.com/'),
+    ('Python.org', 'http://python.org/'),
+    # ('Jinja2', 'http://jinja.pocoo.org/'),
+    # ('You can modify those links in your config file', '#'),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    # ('You can add links in your config file', '#'),
+    # ('Another social link', '#'),
+    ('Twitter', 'https://twitter.com/feigaochn'),
+    # ('Facebook', 'https://facebook.com/feigao.chn'),
+    # ('Instagram', 'http://instagram.com/feigaochn'),
+    # ('LinkedIn', 'http://www.linkedin.com/pub/fei-gao/2b/b36/76a'),
+)
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-STATIC_PATHS = ['images', 'extra/CNAME']
-EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+STATIC_PATHS = [
+    'images',
+    'extra',
+    'extra/CNAME',
+    'extra/README',
+]
+EXTRA_PATH_METADATA = {
+    'extra/CNAME': {'path': 'CNAME'},
+    'extra/README': {'path': 'README.md'},
+}
+
+PAGE_PATHS = ['pages']
+
+THEME = ['simple', 'notmyidea'][1]
+
+DEFAULT_CATEGORY = 'posts'
