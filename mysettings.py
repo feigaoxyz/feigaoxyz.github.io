2b03ad3863c81f0df281f77ca84e7e593ff221b5
Fei Gao <feigao.chn@gmail.com>
2015-01-23 16:31:26 +0800

    work on notmyidea theme

--- mysettings.py
+++ mysettings.py
@@ -1,6 +1,21 @@
+ARTICLE_URL = 'posts/{slug}.html'
+ARTICLE_SAVE_AS = 'posts/{slug}.html'
+# PAGE_URL = 'pages/{slug}/'
+# PAGE_SAVE_AS = 'pages/{slug}/index.html'
+# # prevent from generation
+# CATEGORY_URL = ''
+# # CATEGORY_SAVE_AS = ''
+# AUTHOR_URL = ''
+# # AUTHOR_SAVE_AS = ''
+# TAGS_URL = 'tags/'
+# TAGS_SAVE_AS = 'tags/index.html'
+# ARCHIVES_URL = 'archives/'
+# ARCHIVES_SAVE_AS = 'archives/index.html'
+# TAG_URL = 'tag/{slug}/'
+# TAG_SAVE_AS = 'tag/{slug}/index.html'
+
 STATIC_PATHS = [
     'images',
-    'extra',
     'extra/CNAME',
     'extra/README',
 ]
@@ -9,35 +24,26 @@ EXTRA_PATH_METADATA = {
     'extra/README': {'path': 'README.md'},
 }
 
-# PAGE_PATHS = ['pages']
-
-
 DEFAULT_CATEGORY = 'posts'
 DISPLAY_CATEGORIES_ON_MENU = False
 USE_FOLDER_AS_CATEGORY = False
 
-ARTICLE_URL = 'posts/{slug}/'
-ARTICLE_SAVE_AS = 'posts/{slug}/index.html'
-PAGE_URL = 'pages/{slug}/'
-PAGE_SAVE_AS = 'pages/{slug}/index.html'
-# prevent from generation
-CATEGORY_URL = ''
-# CATEGORY_SAVE_AS = ''
-AUTHOR_URL = ''
-# AUTHOR_SAVE_AS = ''
-TAGS_URL = 'tags/'
-TAGS_SAVE_AS = 'tags/index.html'
-ARCHIVES_URL = 'archives/'
-ARCHIVES_SAVE_AS = 'archives/index.html'
-TAG_URL = 'tag/{slug}/'
-TAG_SAVE_AS = 'tag/{slug}/index.html'
-
+SUMMARY_MAX_LENGTH = 20
 
 MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'toc']
 
-PLUGINS = []
+PLUGIN_PATHS = ['../pelican-plugins']
+PLUGINS = [
+    'render_math',
+    'cjk-auto-spacing',
+]
 
-THEME = ['simple', 'notmyidea', 'elegant', 'themes/mynotmyidea'][-1]
+THEME = ['simple',
+         'notmyidea',
+         'elegant',
+         'themes/mynotmyidea',
+         'themes/pelican-bootstrap3',
+         ][-1]
 
 # settings for notmyidea theme
 # DISQUS_SITENAME = ''
@@ -52,6 +58,6 @@ SOCIAL = (
     ('Weibo', 'http://weibo.com/feigaochn'),
 )
 MENUITEMS = (
-    ('archives', '/archives/'),
-    ('tags', '/tags/')
+    ('archives', '/archives.html'),
+    ('tags', '/tags.html')
 )
